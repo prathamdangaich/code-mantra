1db5793151a4365a888a2b39b181ac6001607b33
Pratham Dangaich <prathamdangaich@gmail.com>
2025-08-14 12:47:21 +0530

    update url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import io from 'socket.io-client'
 import { useState, useEffect, useMemo } from 'react';
 import Editor from '@monaco-editor/react';
 
-const socket = io('http://localhost:3000');
+const socket = io('https://code-mantra-ylgq.onrender.com');
 
 
 
