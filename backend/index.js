3fecc77895e8ae7b15586cd4282483ba7a1b7f3d
Pratham Dangaich <prathamdangaich@gmail.com>
2025-08-14 15:53:55 +0530

    adding setTime to hit the webpage endpoint

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,31 @@ import express from "express";
 import http from 'http';
 import { Server } from "socket.io";
 import path from "path";
+import { version } from "os";
+import axios from "axios";
 
 const app = express();
 
 const server = http.createServer(app);
+
+//Save time while opening again
+const url = `https://code-mantra-ylgq.onrender.com`;
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("website reloded");
+    })
+    .catch((error) => {
+      console.error(`Error : ${error.message}`);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
+
 
 const io = new Server(server, {
     cors: {
@@ -65,6 +86,26 @@ io.on("connection", (socket) => {
 
     socket.on("languageChange", ({roomId, language}) => {
         io.to(roomId).emit("languageUpdate", language);
+    })
+
+    //Adding compiler
+    //Using piston api
+    socket.on("compileCode", async({code, roomId, language, version}) => {
+        if(rooms.has(roomId)){
+            const room = rooms.get(roomId);
+            const response = await axios.post("https://emkc.org/api/v2/piston/execute", {
+                language,
+                version,
+                files: [
+                    {
+                        content: code
+                    }
+                ]
+            })
+
+            room.output = response.data.run.output
+            io.to(roomId).emit("codeResponse", response.data);
+        }
     })
 
     socket.on("disconnect", () => {
